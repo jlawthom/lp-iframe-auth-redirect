10241e0a6cedbf6e3fd29753cada28e0e6029e3d
jlawthom <71012108+jlawthom@users.noreply.github.com>
2021-11-09 20:05:44 +0000

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -36,6 +36,7 @@ document.addEventListener('DOMContentLoaded', function(){
     if(code != null && code != ''){
         // id_token is in the URL, this can only occur in the iframe
         // so send this in a message to the parent
+        console.log(code);
         sendMessage('' + code);
         console.log('11. authCode found in iFrame and called PostMessage');
     }
